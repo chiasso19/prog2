fe56aa307a78214f277f9c6f6b91090aaad3d49d
chiasso19 <83659272+chiasso19@users.noreply.github.com>
2021-12-10 18:26:56 +0100

    html mit python verbunden

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        workout = request.form.get("workout")
+        walk = request.form.get("walk")
+        stretching = request.form.get("stretching")
 
-@app.route("/meine-habits", methods=['GET', 'POST'])
-def meine_habits():
-    if request.method == 'POST':
-        ziel_person = request.form['vorname']
-        rueckgabe_string = "Hello " + ziel_person + "!"
-        return rueckgabe_string
+        vacuum = request.form.get("vacuum")
+        tidy = request.form.get("tidy")
+        bed = request.form.get("bed")
 
-    return render_template('meine_habits.html')
+        veggie = request.form.get("veggie")
+        sweets = request.form.get("sweets")
+        alcohol = request.form.get("alcohol")
+
+        li = [workout, walk, stretching, vacuum, tidy, bed, veggie, sweets, alcohol]
+        return render_template("test.html", listeA=li)   #f"<H1>{workout}, {walk}</H1>"
+    else:
+        return render_template('index.html')
 
 @ app.route("/statistik")
 def statistik():
@@ -24,20 +29,24 @@ def statistik():
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
-"""
-sport = {
-    "Workout": workout_mins,
-    "Walk": walk_mins,
-    "Stretching": stretching_mins
-       }
-haushalt = {
-    "Vaccum": vaccum,
-    "Tidy": tidy,
-    "Make Bed": make_bed
-       }
-nahrung = {
-    "Veggies/Fruits": ,
-    "Sweets": ,
-    "Alcohol":
-       }
 
+"""
+dict_habits = {
+    "sport":[
+        {
+            "workout":20,
+            "walk":20,
+            "stretching":20
+        }],
+    "haushalt":[
+        {"Vacuum": true,
+         "tidy": true,
+         "make_bed": true
+         }],
+    "nahrung":[
+        { viel mittel wenig?????
+
+        }]
+
+}
+"""
