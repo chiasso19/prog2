6a237cdfd544e2c7142c5bde2a99da02c73f6f30
chiasso19 <83659272+chiasso19@users.noreply.github.com>
2022-01-14 18:19:34 +0100

    weitere Berechnungen

--- main.py
+++ main.py
@@ -57,18 +57,20 @@ def statistik():
     sweetsgesamt = 0
     alcoholgesamt = 0
 
-    veggieverg = 0  #Vergleiche
-    sweetsverg = 0  #Vergleiche
-    alcoholverg = 0  #Vergleiche
+    veggieverg = 0 #Vergleich
+    sweetsverg = 0  # Vergleich
+    alcoholverg = 0  # Vergleich
+
+    anzahlhabits = 0 #wie viel Mal wurde getrackt
 
 
     for key, alles in datenhabits.items():
         if alles["Sport"][0]["Workout"] != "":
-            workoutgesamt= workoutgesamt + float(alles["Sport"][0]["Workout"])
+            workoutgesamt= workoutgesamt + int(alles["Sport"][0]["Workout"])
         if alles["Sport"][0]["Walk"] != "":
-            walkgesamt= walkgesamt + float(alles["Sport"][0]["Walk"])
+            walkgesamt= walkgesamt + int(alles["Sport"][0]["Walk"])
         if alles["Sport"][0]["Stretching"] != "":
-            stretchinggesamt= stretchinggesamt + float(alles["Sport"][0]["Stretching"])
+            stretchinggesamt= stretchinggesamt + int(alles["Sport"][0]["Stretching"])
 
         if alles["Haushalt"][0]["Vacuum"]:
             vacuumgesamt = vacuumgesamt + 1
@@ -77,65 +79,48 @@ def statistik():
         if alles["Haushalt"][0]["Bed"]:
             bedgesamt = bedgesamt + 1
 
-        if alles ["Nahrung"][0]["Veggies"]:
-            veggiegesamt = veggiegesamt + 1
+        if alles["Nahrung"][0]["Veggies"]:
+            veggiegesamt = veggiegesamt + 1 #insgesamt
             if alles["Nahrung"][0]["Veggies"] == "viel":#Vergleiche
-                veggieverg = veggieverg + 3
+                veggieverg = veggieverg + 6
             if alles["Nahrung"][0]["Veggies"] == "mittel":#Vergleiche
-                veggieverg = veggieverg + 2
+                veggieverg = veggieverg + 3
             if alles["Nahrung"][0]["Veggies"] == "wenig":#Vergleiche
                 veggieverg = veggieverg + 1
 
-        if alles ["Nahrung"][0]["Sweets"]:
-            sweetsgesamt = sweetsgesamt + 1
+        if alles["Nahrung"][0]["Sweets"]:
+            sweetsgesamt = sweetsgesamt + 1 #insgesamt
             if alles["Nahrung"][0]["Sweets"] == "viel":#Vergleiche
-                sweetsverg = sweetsverg + 3
+                sweetsverg = sweetsverg + 6
             if alles["Nahrung"][0]["Sweets"] == "mittel":#Vergleiche
-                sweetsverg = sweetsverg + 2
+                sweetsverg = sweetsverg + 3
             if alles["Nahrung"][0]["Sweets"] == "wenig":#Vergleiche
                 sweetsverg = sweetsverg + 1
 
-        if alles ["Nahrung"][0]["Alcohol"]:
-            alcoholgesamt = alcoholgesamt + 1
+        if alles["Nahrung"][0]["Alcohol"]:
+            alcoholgesamt = alcoholgesamt + 1 #insgesamt
             if alles["Nahrung"][0]["Alcohol"] == "viel":#Vergleiche
-                alcoholverg = alcoholverg + 3
+                alcoholverg = alcoholverg + 6
             if alles["Nahrung"][0]["Alcohol"] == "mittel":#Vergleiche
-                alcoholverg = alcoholverg + 2
+                alcoholverg = alcoholverg + 3
             if alles["Nahrung"][0]["Alcohol"] == "wenig":#Vergleiche
                 alcoholverg = alcoholverg + 1
 
+        if alles != "":
+            anzahlhabits = anzahlhabits + 1
+
+        vergvegsweet = veggieverg / sweetsverg
+        vergvegalc = veggieverg / alcoholverg
+        vergalcsweet = alcoholverg / sweetsverg
+
+        #monat = str(alles()).split(".")[1]
 
 
-
-    satz_workoutgesamt = "Du hast " + str(workoutgesamt) + " Minuten Workout gmacht."
-    satz_walkgesamt = "Du bist " + str(walkgesamt) + " Minuten gelaufen."
-    satz_stretchinggesamt = "Du hast " + str(stretchinggesamt) + " Minuten Stretching gmacht."
-    satz_vacuumgesamt = "Du hast " + str(vacuumgesamt) + " Mal gestaubsaugt."
-    satz_tidygesamt = "Du hast " + str(tidygesamt) + " Mal aufgeräumt."
-    satz_bedgesamt = "Du hast " + str(bedgesamt) + " dein Bett gemacht."
-    satz_veggiegesamt = "Du hast " + str(veggiegesamt) + " Mal Gemüse gegessen."
-    satz_sweetsgesamt = "Du hast " + str(sweetsgesamt) + " Mal Süsses gegessen."
-    satz_alcoholgesamt = "Du hast " + str(alcoholgesamt) + " Mal Alkohol getrunken."
-    return render_template('statistik.html', satz_workoutgesamt=satz_workoutgesamt, satz_walkgesamt=satz_walkgesamt,satz_stretchinggesamt=satz_stretchinggesamt,satz_vacuumgesamt=satz_vacuumgesamt,satz_tidygesamt=satz_tidygesamt,satz_bedgesamt=satz_bedgesamt,satz_veggiegesamt=satz_veggiegesamt,satz_sweetsgesamt=satz_sweetsgesamt,satz_alcoholgesamt=satz_alcoholgesamt)
-
-
-
-
-
-
-
-
-
-
-
-
-    """for key, value in dicthabits.items():
-        return(key,value)"""
-
-    """for ins_workout in habits.value([['Workout']):
-        ins_workout = "Du hast insgesamt "+ str(ins_workout) + "Minuten Workouts gemacht."""
-    return render_template('statistik.html')
-
+    return render_template('statistik.html', workoutgesamt=workoutgesamt, walkgesamt=walkgesamt,
+                           stretchinggesamt=stretchinggesamt, vacuumgesamt=vacuumgesamt,tidygesamt=tidygesamt,
+                           bedgesamt=bedgesamt, veggiegesamt=veggiegesamt,sweetsgesamt=sweetsgesamt,
+                           alcoholgesamt=alcoholgesamt, vergvegsweet=vergvegsweet, vergvegalc=vergvegalc, vergalcsweet=vergalcsweet,
+                           anzahlhabits=anzahlhabits, monat=monat)
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
